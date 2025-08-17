1719bc1c72ff27ccc91387130ab7542ed6b3fc43
aayush <kaayush163@gmail.com>
2025-08-17 19:08:22 +0530

    two other condition after user authentication done to check if uuser trying to access page of opposite roles admin and user of each other

--- client/src/components/common/check-auth.jsx
+++ client/src/components/common/check-auth.jsx
@@ -3,6 +3,7 @@ import { Navigate, useLocation } from "react-router-dom";
 function CheckAuth({ isAuthenticated, user, children }) {
   const location = useLocation(); // gives cuurent location like auth/login or admin/login
 
+  //user is not authenticated and trying to access any shopping related page then redirect to login
   if (
     !isAuthenticated &&
     !(
@@ -11,10 +12,10 @@ function CheckAuth({ isAuthenticated, user, children }) {
     )
   ) {
     // if user is not authenticated and the path is not login or register page user try to acess any other page without authenticated , redirect to login
-
     return <Navigate to="/auth/login" />;
   }
 
+  //   if user is authenticated and user role is admin then direct to admin dashboard page or else shop/home page
   if (
     isAuthenticated &&
     (location.pathname.includes("/login") ||
@@ -27,6 +28,27 @@ function CheckAuth({ isAuthenticated, user, children }) {
       return <Navigate to="/shop/home" />;
     }
   }
+
+  //   if user is not admin and trying to access admin page then redirect  unauth-page
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.includes("admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
+
+  //   if user is admin and trying to access shoping page then redirect to admin dashboard
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.includes("shop")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
+  }
+
+  //if above nothing is satisfied then return children
+  return <>{children}</>;
 }
 
 export default CheckAuth;
