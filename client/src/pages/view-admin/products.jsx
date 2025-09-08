87a382b3c211f12a8efd11a8abde5739d3a17bb9
aayush <kaayush163@gmail.com>
2025-09-08 08:08:16 +0530

    Sheet, SheetHeader and SheetContent from components/ui taken for products with setting up config addProductFormcontrol

--- client/src/pages/view-admin/products.jsx
+++ client/src/pages/view-admin/products.jsx
@@ -1,5 +1,38 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { Fragment } from "react";
+
 function AdminProducts() {
-  return <div>admin Products</div>;
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+    useState(false);
+  return (
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={() => setOpenCreateProductsDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductsDialog}
+        onOpenChange={() => {
+          setOpenCreateProductsDialog(false);
+        }}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>
+              {currentEditedId !== null ? "Edit Product" : "Add New Product"}
+            </SheetTitle>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </Fragment>
+  );
 }
 
 export default AdminProducts;
