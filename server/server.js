aaf4cb91a3aa04af42f49c776d029c7a6f4add54
aayush <kaayush163@gmail.com>
2025-09-20 14:01:42 +0530

    admin Product app.use added at server file

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+
 const connectDB = require("./configs/db.js");
 require("dotenv").config();
 
@@ -34,5 +36,6 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
