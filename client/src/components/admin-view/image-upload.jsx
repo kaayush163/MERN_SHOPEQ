0eac543529b3c84389c67ad84b1d70b1721ec836
aayush <kaayush163@gmail.com>
2025-09-24 09:18:05 +0530

    Upload image to cloudinary at image-upload.jsx on admin view side

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -46,6 +46,22 @@ function ProductImageUpload({
 
   console.log(imageFile, "imageFile");
 
+  async function uploadImageToCloudinary() {
+    setImageLoadingState(true);
+    const data = new FormData();
+    data.append("my_file", imageFile);
+    const response = await axios.post(
+      "http://localhost:5000/api/admin/products/upload-image",
+      data
+    );
+    console.log(response, "response");
+
+    if (response?.data?.success) {
+      setUploadedImageUrl(response.data.result.url);
+      setImageLoadingState(false);
+    }
+  }
+
   useEffect(() => {
     if (imageFile !== null) uploadImageToCloudinary();
   }, [imageFile]);
