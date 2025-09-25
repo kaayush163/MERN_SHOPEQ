2b97bd091a006e5f8725df5e0066cbc08d48cc94
aayush <kaayush163@gmail.com>
2025-09-25 09:58:12 +0530

    add product added inside the controller

--- server/controllers/admin/products-controller.js
+++ server/controllers/admin/products-controller.js
@@ -20,6 +20,49 @@ const handleImageUpload = async (req, res) => {
   }
 };
 
+//add a new product
+const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+      averageReview,
+    } = req.body;
+
+    console.log(averageReview, "averageReview");
+
+    const newlyCreatedProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+      averageReview,
+    });
+
+    await newlyCreatedProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newlyCreatedProduct,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
 module.exports = {
   handleImageUpload,
   addProduct,
