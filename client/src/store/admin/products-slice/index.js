ca01b572d192a81c5aa82629c54ebba13a136f77
aayush <kaayush163@gmail.com>
2025-09-30 09:54:39 +0530

    createAsyncThunk for addNewProduct

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -4,6 +4,22 @@ const initialState = {
   isLoading: false,
   productList: [],
 };
+export const addNewProduct = createAsyncThunk(
+  "/products/addnewproduct",
+  async (formData) => {
+    const result = await axios.post(
+      "http://localhost:5000/api/admin/products/add",
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return result?.data;
+  }
+);
 
 const AdminProductsSlice = createSlice({
   name: "adminProducts",
