a0991ddc6ceed65aa87f593a523f802ccae741de
aayush <kaayush163@gmail.com>
2025-10-01 14:46:41 +0530

    edit Product createAsyncThunk added in store->admin->product-slice

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -33,6 +33,23 @@ export const fetchAllProducts = createAsyncThunk(
   }
 );
 
+export const editProduct = createAsyncThunk(
+  "/products/editProduct",
+  async ({ id, formData }) => {
+    const result = await axios.put(
+      `http://localhost:5000/api/admin/products/edit/${id}`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return result?.data;
+  }
+);
+
 const AdminProductsSlice = createSlice({
   name: "adminProducts",
   initialState,
