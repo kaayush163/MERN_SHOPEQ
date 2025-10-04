2c7badfa972409335b00f9bf6c463a3f3b3fba17
aayush <kaayush163@gmail.com>
2025-10-04 21:57:46 +0530

    isFormValid in products page of view-admin with dispatching editProduct also on form submitting

--- client/src/pages/view-admin/products.jsx
+++ client/src/pages/view-admin/products.jsx
@@ -7,7 +7,11 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
-import { addNewProduct, fetchAllProducts } from "@/store/admin/products-slice";
+import {
+  addNewProduct,
+  editProduct,
+  fetchAllProducts,
+} from "@/store/admin/products-slice";
 import { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -40,21 +44,47 @@ function AdminProducts() {
   function onSubmit(event) {
     event.preventDefault();
 
-    dispatch(addNewProduct({ ...formData, image: uploadedImageUrl })).then(
-      (data) => {
-        console.log(data);
-        if (data?.payload?.success) {
-          setOpenCreateProductsDialog(false);
-          setFormData(initialFormData);
-          setUploadedImageUrl("");
-          setImageFile(null);
-          dispatch(fetchAllProducts());
-          toast({
-            title: "Product add successfully",
-          });
-        }
-      }
-    );
+    // on edit and subbmit the display will shwo the updated details of Product
+    currentEditedId !== null
+      ? dispatch(
+          // passing same parameters where this edit Product defined on creating its createAsyncThunk
+          editProduct({
+            id: currentEditedId,
+            formData,
+          })
+        ).then((data) => {
+          console.log(data, "edit");
+
+          if (data?.payload?.success) {
+            dispatch(fetchAllProducts());
+            setFormData(initialFormData);
+            setOpenCreateProductsDialog(false);
+            setCurrentEditedId(null);
+          }
+        })
+      : dispatch(
+          addNewProduct({
+            ...formData,
+            image: uploadedImageUrl,
+          })
+        ).then((data) => {
+          if (data?.payload?.success) {
+            dispatch(fetchAllProducts());
+            setOpenCreateProductsDialog(false);
+            setImageFile(null);
+            setFormData(initialFormData);
+            toast({
+              title: "Product add successfully",
+            });
+          }
+        });
+  }
+
+  function isFormValid() {
+    return Object.keys(formData)
+      .filter((currentKey) => currentKey !== "averageReview")
+      .map((key) => formData[key] !== "")
+      .every((item) => item);
   }
 
   useEffect(() => {
