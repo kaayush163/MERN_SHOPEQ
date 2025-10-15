9dff74689f7f62101bf487c993f5c977673fed98
aayush <kaayush163@gmail.com>
2025-10-15 22:23:24 +0530

    shopinglisting and product filter basic build

---
+++ client/src/components/shopping-view/filter.jsx
@@ -0,0 +1,18 @@
+import { filterOptions } from "@/config";
+import { Fragment } from "react";
+import { Label } from "../ui/label";
+import { Checkbox } from "../ui/checkbox";
+import { Separator } from "../ui/separator";
+
+function ProductFilter() {
+  return (
+    <div className="bg-background rounded-lg shadow-sm">
+      <div className="p-4 border-b">
+        <h2 className="text-lg font-extrabold">Filters</h2>
+      </div>
+      <div className="p-4 space-y-4"></div>
+    </div>
+  );
+}
+
+export default ProductFilter;
