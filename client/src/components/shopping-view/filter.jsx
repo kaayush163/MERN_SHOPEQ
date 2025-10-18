55326e1dcef414782b60239d0b6f16393dd3c557
aayush <kaayush163@gmail.com>
2025-10-18 15:27:04 +0530

    filter section building started

--- client/src/components/shopping-view/filter.jsx
+++ client/src/components/shopping-view/filter.jsx
@@ -10,7 +10,16 @@ function ProductFilter() {
       <div className="p-4 border-b">
         <h2 className="text-lg font-extrabold">Filters</h2>
       </div>
-      <div className="p-4 space-y-4"></div>
+      <div className="p-4 space-y-4">
+        {Object.keys(filterOptions).map((keyItem) => (
+          <Fragment>
+            <div>
+              <h3 className="text-base font-bold">{keyItem}</h3>
+            </div>
+            <Separator />
+          </Fragment>
+        ))}
+      </div>
     </div>
   );
 }
