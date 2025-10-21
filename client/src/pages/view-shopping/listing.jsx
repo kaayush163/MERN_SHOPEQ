7013686774c27668ece230bf93c418f1d3d26e82
aayush <kaayush163@gmail.com>
2025-10-21 21:12:30 +0530

    all product showcase with tailwind css style

--- client/src/pages/view-shopping/listing.jsx
+++ client/src/pages/view-shopping/listing.jsx
@@ -4,6 +4,28 @@ function ShoppingListing() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
       <ProductFilter filters={filters} handleFilter={handleFilter} />
+
+      <div className="bg-background w-full rounded-lg shadow-sm">
+        <div className="p-4 border-b flex items-center justify-between">
+          <h2 className="text-lg font-extrabold">All Products</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-muted-foreground">
+              {productList?.length} Products
+            </span>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+          {productList && productList.length > 0
+            ? productList.map((productItem) => (
+                <ShoppingProductTile
+                  handleGetProductDetails={handleGetProductDetails}
+                  product={productItem}
+                  handleAddtoCart={handleAddtoCart}
+                />
+              ))
+            : null}
+        </div>
+      </div>
     </div>
   );
 }
