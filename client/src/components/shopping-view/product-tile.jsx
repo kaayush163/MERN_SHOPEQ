ad0572548c593e67c5acee0e2426f233e9fafd19
aayush <kaayush163@gmail.com>
2025-10-26 09:11:47 +0530

    Card and Badge added for product-tile in shoping view component

--- client/src/components/shopping-view/product-tile.jsx
+++ client/src/components/shopping-view/product-tile.jsx
@@ -1,7 +1,33 @@
+import { Badge } from "../ui/badge";
+import { Card } from "../ui/card";
 
-
-function ShoppingProductTile() {
-    return();
+function ShoppingProductTile({ product }) {
+  return (
+    <Card className="w-full max-w-sm mx-auto">
+      <div>
+        <div className="relative">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="w-full h-[300px] object-cover rounded-t-lg"
+          />
+          {product?.totalStock === 0 ? (
+            <Badge className="absolute top-2 left-2 bg-red-500 hover:bg-red-600">
+              Out Of Stock
+            </Badge>
+          ) : product?.totalStock < 10 ? (
+            <Badge className="absolute top-2 left-2 bg-red-500 hover:bg-red-600">
+              {`Only ${product?.totalStock} items left`}
+            </Badge>
+          ) : product?.salePrice > 0 ? (
+            <Badge className="absolute top-2 left-2 bg-red-500 hover:bg-red-600">
+              Sale
+            </Badge>
+          ) : null}
+        </div>
+      </div>
+    </Card>
+  );
 }
 
-export default ShoppingProductTile
+export default ShoppingProductTile;
