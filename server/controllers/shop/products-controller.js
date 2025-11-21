e40353a919db777d108d6a3fe99fb1b6659fdd36
aayush <kaayush163@gmail.com>
2025-11-21 08:38:02 +0530

    sortBy on getFilteredProducts at server controller

--- server/controllers/shop/products-controller.js
+++ server/controllers/shop/products-controller.js
@@ -2,7 +2,43 @@ const Product = require("../../models/Product");
 
 const getFilteredProducts = async (req, res) => {
   try {
+    // sortBy by default price-lowtohigh
+    const { category = [], brand = [], sortBy = "price-lowtohigh" } = req.query;
+    // req query will contain search params whatever filtered will show on url as well
+
     let filters = {};
+
+    // category need to split by , because multiple categories can be selected
+    // If the field holds an array, then the $in operator selects the documents whose field holds an array that contains at least one element that matches a value in the specified array (for example, <value1> , <value2> , and so on).
+    if (category.length) {
+      filters.category = { $in: category.split(",") };
+    }
+
+    if (brand.length) {
+      filters.brand = { $in: brand.split(",") };
+    }
+
+    let sort = {};
+    // i decreasing order then -1 sort,price = -1
+    switch (sortBy) {
+      case "price-lowtohigh":
+        sort.price = 1;
+        break;
+      case "price-hightolow":
+        sort.price = -1;
+        break;
+      case "title-atoz":
+        sort.title = 1;
+        break;
+
+      case "title-ztoa":
+        sort.title = -1;
+        break;
+
+      default:
+        sort.price = 1;
+        break;
+    }
 
     const products = await Product.find(filters).sort(sort);
 
