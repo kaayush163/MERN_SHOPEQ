1b70184e8ddb52e3d43d948480a763b40bcb468d
aayush <kaayush163@gmail.com>
2025-12-09 10:08:21 +0530

    served the routes of cart at main backend file

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
-
 const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
 
 const connectDB = require("./configs/db.js");
 require("dotenv").config();
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
-
 app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
