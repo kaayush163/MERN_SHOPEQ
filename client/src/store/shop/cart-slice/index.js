a917b122ff07d79bca83c20fa1aaa348f0a3274a
aayush <kaayush163@gmail.com>
2025-12-11 08:52:52 +0530

    Store/shop/cart-slice createAsyncThunk function for addToCart with defining its createSlice method with three extraReducers

---
+++ client/src/store/shop/cart-slice/index.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  isLoading: false,
+};
+
+export const addToCart = createAsyncThunk(
+  "cart/addToCart",
+  async ({ userId, productId, quantity }) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/shop/cart/add",
+      {
+        userId,
+        productId,
+        quantity,
+      }
+    );
+
+    return response.data;
+  }
+);
+
+const shoppingCartSlice = createSlice({
+  name: "shoppingCart",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(addToCart.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(addToCart.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.cartItems = action.payload.data;
+      })
+      .addCase(addToCart.rejected, (state) => {
+        state.isLoading = false;
+        state.cartItems = [];
+      });
+  },
+});
+
+export default shoppingCartSlice.reducer;
