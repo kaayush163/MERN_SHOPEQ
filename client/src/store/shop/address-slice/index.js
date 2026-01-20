bdfd195ec6cfd14496722c8bb8acad25f7480d56
aayush <kaayush163@gmail.com>
2026-01-21 00:38:26 +0530

    addNewAddress createAsyncThunk created in addressSlice in shop store

--- client/src/store/shop/address-slice/index.js
+++ client/src/store/shop/address-slice/index.js
@@ -6,6 +6,19 @@ const initialState = {
   addressList: [],
 };
 
+export const addNewAddress = createAsyncThunk(
+  "/addresses/addNewAddress",
+  async (formData) => {
+    const response = await axios.post(
+      // same as from server.js and routes of add address
+      "http://localhost:5000/api/shop/address/add",
+      formData
+    );
+
+    return response.data;
+  }
+);
+
 const addressSlice = createSlice({
   name: "address",
   initialState,
