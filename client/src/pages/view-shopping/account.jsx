491dfc93a70e1d319262194b9ffade9a7c2721dd
aayush <kaayush163@gmail.com>
2026-01-26 12:55:58 +0530

    Shoping Account page started built with accImagdefined first in view-shoping page

--- client/src/pages/view-shopping/account.jsx
+++ client/src/pages/view-shopping/account.jsx
@@ -1,5 +1,19 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import accImg from "../../assets/account.jpg";
+import Address from "@/components/shopping-view/address";
+import ShoppingOrders from "@/components/shopping-view/orders";
+
 function ShoppingAccount() {
-  return <div>shopping view Account</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[300px] w-full overflow-hidden">
+        <img
+          src={accImg}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ShoppingAccount;
