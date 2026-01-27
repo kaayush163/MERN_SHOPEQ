6ef84f57b66b18e6ec6284342e93c86be8594953
aayush <kaayush163@gmail.com>
2026-01-27 10:25:32 +0530

    starting basic building for AddressCard component of shoping-view part

--- client/src/components/shopping-view/address.jsx
+++ client/src/components/shopping-view/address.jsx
@@ -1,5 +1,34 @@
-function Address() {
-  return <div></div>;
+import CommonForm from "../common/form";
+import { Button } from "../ui/button";
+import { Card, CardContent, CardFooter } from "../ui/card";
+import { Label } from "../ui/label";
+
+const initialAddressFormData = {
+  address: "",
+  city: "",
+  phone: "",
+  pincode: "",
+
+  notes: "",
+};
+function AddressCard({
+  addressInfo,
+  handleDeleteAddress,
+  handleEditAddress,
+  setCurrentSelectedAddress,
+  selectedId,
+}) {
+  const [formData, setFormData] = useState(initialAddressFormData);
+  return (
+    <Card>
+      <CardHeader>
+        <CartTitle> Add New Address</CartTitle>
+      </CardHeader>
+      <CardContent classname="space-y-3">
+        <CommonForm />
+      </CardContent>
+    </Card>
+  );
 }
 
-export default Address;
+export default AddressCard;
