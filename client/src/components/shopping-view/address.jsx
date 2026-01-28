ef188bc233bda4897dffa4ba707bab628c1f8033
aayush <kaayush163@gmail.com>
2026-01-28 10:23:12 +0530

    handleManageAddress fucntionality created shoping-view/address component

--- client/src/components/shopping-view/address.jsx
+++ client/src/components/shopping-view/address.jsx
@@ -1,36 +1,87 @@
-import { addressFormControls } from "@/config";
+import { useEffect, useState } from "react";
 import CommonForm from "../common/form";
-import { Button } from "../ui/button";
-import { Card, CardContent, CardFooter } from "../ui/card";
-import { Label } from "../ui/label";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { addressFormControls } from "@/config";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addNewAddress,
+  deleteAddress,
+  editaAddress,
+  fetchAllAddresses,
+} from "@/store/shop/address-slice";
+import AddressCard from "./address-card";
+import { useToast } from "../ui/use-toast";
 
 const initialAddressFormData = {
   address: "",
   city: "",
   phone: "",
   pincode: "",
-
   notes: "",
 };
-function AddressCard({
-  addressInfo,
-  handleDeleteAddress,
-  handleEditAddress,
-  setCurrentSelectedAddress,
-  selectedId,
-}) {
+
+function Address({ setCurrentSelectedAddress, selectedId }) {
   const [formData, setFormData] = useState(initialAddressFormData);
+
+  function handleManageAddress(event) {
+    event.preventDefault();
+
+    if (addressList.length >= 3 && currentEditedId === null) {
+      setFormData(initialAddressFormData);
+      toast({
+        title: "You can add max 3 addresses",
+        variant: "destructive",
+      });
+
+      return;
+    }
+
+    currentEditedId !== null
+      ? dispatch(
+          editaAddress({
+            userId: user?.id,
+            addressId: currentEditedId,
+            formData,
+          }),
+        ).then((data) => {
+          if (data?.payload?.success) {
+            dispatch(fetchAllAddresses(user?.id));
+            setCurrentEditedId(null);
+            setFormData(initialAddressFormData);
+            toast({
+              title: "Address updated successfully",
+            });
+          }
+        })
+      : dispatch(
+          addNewAddress({
+            ...formData,
+            userId: user?.id,
+          }),
+        ).then((data) => {
+          if (data?.payload?.success) {
+            dispatch(fetchAllAddresses(user?.id));
+            setFormData(initialAddressFormData);
+            toast({
+              title: "Address added successfully",
+            });
+          }
+        });
+  }
+
   return (
     <Card>
       <CardHeader>
-        <CartTitle> Add New Address</CartTitle>
+        <CardTitle>
+          {currentEditedId !== null ? "Edit Address" : "Add New Address"}
+        </CardTitle>
       </CardHeader>
-      <CardContent classname="space-y-3">
+      <CardContent className="space-y-3">
         <CommonForm
           formControls={addressFormControls}
           formData={formData}
           setFormData={setFormData}
-          buttonText={"Add"}
+          buttonText={currentEditedId !== null ? "Edit" : "Add"}
           onSubmit={handleManageAddress}
         />
       </CardContent>
@@ -38,4 +89,4 @@ function AddressCard({
   );
 }
 
-export default AddressCard;
+export default Address;
