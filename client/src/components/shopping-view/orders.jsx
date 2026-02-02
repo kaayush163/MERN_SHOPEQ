559ea3f70c472337c467051ab26ee4e4aa243ebd
aayush <kaayush163@gmail.com>
2026-02-02 09:25:00 +0530

    Table content column created

--- client/src/components/shopping-view/orders.jsx
+++ client/src/components/shopping-view/orders.jsx
@@ -1,5 +1,26 @@
 function ShoppingOrders() {
-  return <div></div>;
+  eturn(
+    <Card>
+      <CardHeader>
+        <CardTitle>Order History</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Order ID</TableHead>
+              <TableHead>Order Date</TableHead>
+              <TableHead>Order Status</TableHead>
+              <TableHead>Order Price</TableHead>
+              <TableHead>
+                <span className="sr-only">Details</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+        </Table>
+      </CardContent>
+    </Card>,
+  );
 }
 
 export default ShoppingOrders;
