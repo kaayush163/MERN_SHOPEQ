658bf2d57318141b68bb75e76760a941eb87a002
aayush <kaayush163@gmail.com>
2026-03-13 07:46:19 +0530

    paypal-return page of vie-shoping component started

---
+++ client/src/pages/view-shopping/paypal-return.jsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { capturePayment } from "@/store/shop/order-slice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+
+function PaypalReturnPage() {
+  return <Card></Card>;
+}
+
+export default PaypalReturnPage;
