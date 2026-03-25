25ee3088b559dc3abb94bb534283ce74ac357c06
aayush <kaayush163@gmail.com>
2026-03-25 22:31:08 +0530

    getOrderDetails functionality added in order-controller server side

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -175,8 +175,35 @@ const getAllOrdersByUser = async (req, res) => {
   }
 };
 
+const getOrderDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: order,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
 module.exports = {
   createOrder,
   capturePayment,
   getAllOrdersByUser,
+  getOrderDetails,
 };
