9e90d84262ecb7a43bf416796e82ede9e1b71a10
aayush <kaayush163@gmail.com>
2026-04-05 15:25:48 +0530

    routes added getOrderDetailsForAdmin

--- server/routes/admin/order-routes.js
+++ server/routes/admin/order-routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const {
   getAllOrdersOfAllUsers,
+  getOrderDetailsForAdmin,
 } = require("../../controllers/admin/order-controller");
 
 const router = express.Router();
 
 router.get("/get", getAllOrdersOfAllUsers);
+router.get("/details/:id", getOrderDetailsForAdmin);
 
 module.exports = router;
