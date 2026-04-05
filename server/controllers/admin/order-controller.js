605f57714148ae2028deb66c80f1d74ae0cef24f
aayush <kaayush163@gmail.com>
2026-04-05 15:24:38 +0530

    getOrderDetailsForAdmin functionality added in order-controller

--- server/controllers/admin/order-controller.js
+++ server/controllers/admin/order-controller.js
@@ -24,4 +24,30 @@ const getAllOrdersOfAllUsers = async (req, res) => {
   }
 };
 
-module.exports = { getAllOrdersOfAllUsers };
+const getOrderDetailsForAdmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: order,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+module.exports = { getAllOrdersOfAllUsers, getOrderDetailsForAdmin };
