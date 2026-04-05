2b1e6862950d6bde07a6839cbb650adbdc77de6f
aayush <kaayush163@gmail.com>
2026-04-06 01:39:18 +0530

    adminOrderSlice created in store with createAsyncThunk Methods to handle diff. requests

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,17 +2,24 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./auth-slice";
 import adminProductsSlice from "@/pages/view-admin/products";
+import adminOrderSlice from "./admin/order-slice";
+
 import shopProductsSlice from "./shop/products-slice";
 import shopCartSlice from "./shop/cart-slice";
 import shopAddressSlice from "./shop/address-slice";
+import shopOrderSlice from "./shop/order-slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+
     adminProducts: adminProductsSlice,
+    adminOrder: adminOrderSlice,
+
     shopProducts: shopProductsSlice,
     shopCart: shopCartSlice,
     shopAddress: shopAddressSlice,
+    shopOrder: shopOrderSlice,
   },
 });
 
